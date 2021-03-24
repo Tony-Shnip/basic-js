07fdfbba66f2ea7f7f4c8a8076783e4811cbbf7b
Tony-Shnip <shnip12345@gmail.com>
2021-03-24 23:22:16 +0300

    feat: dream-team completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,24 +4,16 @@ module.exports = function createDreamTeam(members) {
   if(!(Array.isArray(members)))return false;
   let team = [];
   for(let i = 0; i < members.length; i++){
-    if(typeof members[i] === "string")team.push(members[i][0])
+    if(typeof members[i] === "string")team.push(members[i])
   }
-  for(let i = 0; i < team.length; i++){
-    if(Array.isArray(team[i])){
-      for(let j = 0; j < team[i][0].length; j++){
-        if(team[i][0][j] !== ' ') { team[i] == team[i][0][j]; break; }
-      }
+  for(i = 0; i < team.length; i++){
+    for(let j = 0; j < team[i].length; j++){
+      if(team[i][j] != ' '){ team[i] = team[i][j]; break; }
     }
-    else {
-      for(let j = 0; j < team[i].length; j++){
-        if(team[i][j] !== ' '){ team[i] == team[i][j]; break; }
-    }
-    }
-
   }
-  for(let i = 0; i < team.length; i++){
+  for(i = 0; i < team.length; i++){
     team[i] = team[i].toUpperCase();
   }
   team.sort();
-  return team;
+  return team.join('');
 };

f7c4216962f7bde9c2a54257bcf548a7b5a4ae8f
Tony-Shnip <shnip12345@gmail.com>
2021-03-25 21:01:56 +0300

    feat: recursive-depth completed

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, maxd = [], curd = 1) {
+    maxd.push(curd);
+    for(let i = 0; i < arr.length; i++){
+      if(Array.isArray(arr[i])){ this.calculateDepth(arr[i], maxd, curd + 1);}
+    }
+    let ans = 0;
+    for(let i = 0; i < maxd.length; i++){
+      if(maxd[i] > ans) ans = maxd[i];
+    }
+    return ans;
   }
 };
